552a5ecce1905854f19efc35a231dc7565b1dd65
Gulzat Karimova <gulzat.karimova@gmail.com>
2019-02-13 13:20:37 -0800

    Added jsx looping, linting and used css module

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,19 +1,40 @@
-import React from "react";
-import Ticket from "./Ticket";
+import React from 'react'
+import Ticket from './Ticket'
+
+let masterTicketList = [
+  {
+    location: '3A',
+    names: 'Thato and Haley',
+    issue: 'Firebase will not save record!' 
+  },
+  {
+    location: '4B',
+    names: 'Gulzat and Jibek',
+    issue: 'Computer does not boot!'
+
+  },
+  {
+    location: '9F',
+    names: 'Sleater and Kinney',
+    issue: 'Fox image not displaying on page, can only see duck?'
+
+  }
+]
 
 function TicketList() {
-    return (
-        <div>
-            <Ticket
-                location="3A"
-                names="Thato and Haley"
-                issue="Firebase will not save record!" />
-            <Ticket
-                location="4B"
-                names="Gulzat and Jibek"
-                issue="Computer does not boot!" />
-        </div>
-    );
+  return (
+    <div>
+      <hr/>
+      {
+        masterTicketList.map((ticket, index) =>
+          <Ticket names={ticket.names}
+            location={ticket.location}
+            issue={ticket.issue} 
+            key={index} />
+       
+        )}
+    </div>
+  )
 }
 
-export default TicketList;
+export default TicketList
